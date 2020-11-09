4b07051bc15d10a1aa75254959512ad34388ef6f
abhinavreddy <balabhinavreddy@gmail.com>
2020-11-09 20:10:46 +0530

    maps

--- src/app/googlemaps/googlemaps.component.ts
+++ src/app/googlemaps/googlemaps.component.ts
@@ -22,6 +22,33 @@ export class GooglemapsComponent implements OnInit {
     
 
     var mymap = L.map('mapid').setView([51.505, -0.09], 13);
+    var someFeatures = [{
+      "type": "Feature",
+      "properties": {
+          "name": "Coors Field",
+          "show_on_map": true
+      },
+      "geometry": {
+          "type": "Point",
+          "coordinates": [-104.99404, 39.75621]
+      }
+  }, {
+      "type": "Feature",
+      "properties": {
+          "name": "Busch Field",
+          "amenity": "Baseball Stadium",
+          "show_on_map": false
+      },
+      "geometry": {
+          "type": "polygon",
+          "coordinates": [51.505, -0.10]
+      }
+  }];
+  L.geoJSON(someFeatures, {
+    filter: function(feature, layer) {
+        return feature.properties.show_on_map;
+    }
+}).addTo(mymap);
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
       attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(mymap);
